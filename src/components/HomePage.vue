571ab5c1f0d866d4691fcaf8947603b48fb7ab11
Jacques Ehrardt <ehrardt38@gmail.com>
2024-01-02 13:33:08 -0800

    Add HomePage and WeatherComponent

---
+++ src/components/HomePage.vue
@@ -0,0 +1,79 @@
+<template>
+   <div id="cityCarousel" class="carousel slide">
+      <div class="carousel-inner">
+         <div 
+            class="carousel-item" 
+            v-for="city in cityList" :key="city.id"
+            :class="[city.id===1 ? 'active' : '']"
+         >
+            <WeatherComponent :city="city"/>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+   import WeatherComponent from './WeatherComponent.vue';
+
+   export default {
+      components: {
+        WeatherComponent
+      },
+
+      data(){
+         return {
+            cityList: [
+               {
+                  id: 1,
+                  name: 'Vancouver',
+                  temp: 4,
+                  state: 'Sunny',
+                  feelsLike: 3,
+                  wind: 39,
+                  humidity: 40,
+               },
+               {
+                  id: 2,
+                  name: 'Maceio',
+                  temp: 29,
+                  state: 'Cloud',
+                  feelsLike: 32,
+                  wind: 29,
+                  humidity: 50,
+               },
+               {
+                  id: 3,
+                  name: 'Viena',
+                  state: 'Rain',
+                  temp: 15,
+                  feelsLike: 16,
+                  wind: 59,
+                  humidity: 60,
+               }
+            ],
+         }
+      },
+
+      props: {
+         
+      },
+   }
+</script>
+
+<style>
+   div{
+    height: 100vh;
+  }
+
+   #carouselExample{
+      width: 100%;
+      height: 100%;
+      background: #000;
+      display: flex;
+      align-items: center;
+   }
+
+   .carousel-control-prev, .carousel-control-next{
+      position: relative;
+   }
+</style>
---
+++ src/components/WeatherComponent.vue
@@ -0,0 +1,58 @@
+<template>
+   <main>
+      <section>
+         <section>
+            <button 
+               class="carousel-control-prev" 
+               type="button" 
+               data-bs-target="#cityCarousel" 
+               data-bs-slide="prev"
+            >
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+            </button>
+            <article>
+               <h1>{{ city.name }}</h1>
+               <h3>{{ city.state }}</h3>
+               <h2>{{ city.temp }} &deg;C</h2>
+            </article>
+            <button 
+               class="carousel-control-next" 
+               type="button"
+               data-bs-target="#cityCarousel" 
+               data-bs-slide="next"
+            >
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+            </button>
+         </section>
+      </section>
+      <article>
+         <aside>
+            <h4>Feels Like</h4>
+            <p>{{ city.feelsLike}} &deg;C</p>
+         </aside>
+         <aside>
+            <h4>Wind</h4>
+            <p>{{ city.wind}}km/h</p>
+         </aside>
+         <aside>
+            <h4>Humidity</h4>
+            <p>{{ city.humidity}}%</p>
+         </aside>
+      </article>
+   </main>
+</template>
+
+<script>
+export default ({
+   name: 'WeatherComponent',
+   props: {
+      city: {},
+   },
+})
+</script>
+
+<style>
+   
+</style>
