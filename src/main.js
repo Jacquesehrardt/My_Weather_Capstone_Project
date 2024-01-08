4264db9333c41add6c16905e65d3d9bcd317d380
Jacques Ehrardt <ehrardt38@gmail.com>
2024-01-07 22:11:33 -0800

    Import Fontawesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/js/bootstrap.js'
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faPlus, faEllipsisVertical)
+
+createApp(App)
+.component('FontAwesomeIcon', FontAwesomeIcon)
+.mount('#app')
