9a615901009214a3592f0218e3e0a579fa9f2c55
Jacques Ehrardt <ehrardt38@gmail.com>
2024-01-12 13:52:26 -0800

    Add functionalities: go to addCity and logout

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
    <header>
-      <button>
+      <button @click="addCityPage">
          <FontAwesomeIcon :icon="['fas', 'plus']" />
       </button>
       <div class="carousel-indicators" id="indicators">
@@ -14,18 +14,37 @@
             :class="[index===1 ? 'active' : '']"
          ></button>
       </div>
-      <button>
-         <FontAwesomeIcon :icon="['fas', 'ellipsis-vertical']" />
-      </button>
+      <div class="dropdown">
+         <button type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <FontAwesomeIcon :icon="['fas', 'ellipsis-vertical']" />
+         </button>
+         <ul class="dropdown-menu">
+            <li><button @click="logout()" type="button" class="btn btn-danger">logout</button></li>
+         </ul>
+      </div>
    </header>
 </template>
 
 <script>
+import VueCookies from 'vue-cookies';
+
 export default ({
    name: 'HeaderComponent',
    props: {
       elementsNumber: Number,
    },
+   methods:{
+      addCityPage(){
+         window.location.replace("/addcity")
+      },
+      logout() {
+			if (VueCookies.isKey("session")) {
+				VueCookies.remove("session");
+				this.logged = false;
+            window.location.replace("/");
+			}
+		}
+   }
 })
 </script>
    
@@ -35,14 +54,23 @@ export default ({
       justify-content: space-between;
    }
 
-   header > button{
+   header > button,
+   .dropdown > button{
       height: 5vh;
       width: 5vh;
       margin: 0;
       padding: 0;
       background-color: transparent;
       border: none;
+      border-radius: 8px;
       font-size: 20px;
+      transition: .5s;
+   }
+
+   header > button:hover,
+   .dropdown > button:hover{
+      color: white;
+      background-color: #66adffa2;
    }
 
    #indicators{
